00604871d16132807b0b9689577995f73f03b017
Ravi Shankar Singh <ravi13eng@gmail.com>
2020-08-09 16:52:52 +0530

    Updates

---
+++ precache-manifest.d17fe5c4323149e7e0223156a02d3598.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80cad0a14a4139e67cd2a355fa96eec4",
+    "url": "/tooltip/index.html"
+  },
+  {
+    "revision": "a087e5828887787c59fb",
+    "url": "/tooltip/static/css/main.c21c8222.chunk.css"
+  },
+  {
+    "revision": "de3778446fa07db8943c",
+    "url": "/tooltip/static/js/2.fa86821f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tooltip/static/js/2.fa86821f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a087e5828887787c59fb",
+    "url": "/tooltip/static/js/main.0a496738.chunk.js"
+  },
+  {
+    "revision": "6d81586e19be32d511a4",
+    "url": "/tooltip/static/js/runtime-main.ca1d1748.js"
+  }
+]);
